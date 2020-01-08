6e0afd43889c0590d37475181f3b38f30109a2cd
Hesels Christian <s0553310@htw-berlin.de>
2020-01-08 07:26:24 +0100

    Webapp init
    	- Removed old frontend
    	- Removed old frontend from docker-compose
    	- Added NuxtJS project as webapp

--- 1/frontend/src/components/AddMission.spec.js
+++
@@ -1,9 +0,0 @@
-import {mount} from '@vue/test-utils'
-import AddMission from '@/components/AddMission.vue'
-
-describe('AddMission', () => {
-    test('is a Vue instance', () => {
-        const wrapper = mount(AddMission)
-        expect(wrapper.isVueInstance()).toBeTruthy()
-    })
-})
